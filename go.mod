10be4082b8792db1f3530fcdb2f5f45a94531145
alexismansilla <alexis.mansilla04@gmail.com>
2020-06-02 23:40:46 -0400

    Add the gin library.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alexismansilla/ad-poker
 
 go 1.14
+
+require github.com/gin-gonic/gin v1.6.3
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "Hi",
+		})
+	})
+	router.Run(":3000")
+
 	fmt.Println("Holamundo")
 }
