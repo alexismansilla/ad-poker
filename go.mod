437ca38f33bba98953c7dc5202ab9ed7ee8aa4a7
alexismansilla <alexis.mansilla04@gmail.com>
2020-06-03 00:54:38 -0400

    [DRAF] add template

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alexismansilla/ad-poker
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/foolin/gin-template v0.0.0-20190415034731-41efedfb393b
+	github.com/gin-gonic/gin v1.6.3
+)
--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/foolin/gin-template"
+	gintemplate "github.com/foolin/gin-template"
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Name string
+}
+
 func main() {
 	router := gin.Default()
+
+	//new template engine
+	router.HTMLRender = gintemplate.Default()
+
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "Hi",
-		})
+		c.JSON(200, gin.H{"status": "Hi"})
+	})
+
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(200, "templates/index.html", gin.H{"title": "Hola"})
 	})
 	router.Run(":3000")
-
-	fmt.Println("Holamundo")
 }
