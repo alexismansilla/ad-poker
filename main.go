6e64059c9b83f18ac2fe08503cb2c1131a948655
alexismansilla <alexis.mansilla04@gmail.com>
2020-06-03 22:16:55 -0400

    Add index to template folder

--- main.go
+++ main.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"github.com/foolin/gin-template"
-	gintemplate "github.com/foolin/gin-template"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	Name string
-}
-
 func main() {
 	router := gin.Default()
-
-	//new template engine
-	router.HTMLRender = gintemplate.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "Hi"})
 	})
 
 	router.GET("/index", func(c *gin.Context) {
-		c.HTML(200, "templates/index.html", gin.H{"title": "Hola"})
+		c.HTML(http.StatusOK, "index.html", gin.H{"title": "Hola"})
 	})
 	router.Run(":3000")
 }
